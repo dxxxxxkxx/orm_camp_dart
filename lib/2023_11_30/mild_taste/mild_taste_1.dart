c6b60babda70ac3bccff837f9e3fbf692e310f91
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-11-30 21:42:05 +0900

    순핫 맛 1, 2

---
+++ lib/2023_11_30/mild_taste/mild_taste_1.dart
@@ -0,0 +1,12 @@
+void main() {
+  int a = 3;
+  int b = 4;
+
+  if (a > b) {
+    print(a);
+  } else if (a < b) {
+    print(b);
+  } else {
+    print('eq'); // a == b
+  }
+}
---
+++ lib/2023_11_30/mild_taste/mild_taste_2.dart
@@ -0,0 +1,33 @@
+void main() {
+  int m = 30;
+  int n = 15;
+
+  m %= 2;
+  n %= 2;
+
+  /*
+  Caution: Dart supports short-circuit evaluation (lazy evaluation).
+  ex) if ((m == 0 && n == 1) || (m == 0 && n == 0)) {}
+      (m == 0 && n == 0) is not executed.
+   */
+
+  bool isChecked1 = false;
+  bool isChecked2 = false;
+
+  isChecked1 = (m == 0 && n == 1);
+  isChecked2 = (m == 1 && n == 0);
+
+  if (isChecked1) {
+    if (!isChecked2) {
+      print('YES');
+    } else {
+      print('NO');
+    }
+  } else if (!isChecked1) {
+    if (isChecked2) {
+      print('YES');
+    } else {
+      print('NO');
+    }
+  }
+}
