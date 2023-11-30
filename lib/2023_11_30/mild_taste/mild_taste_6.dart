64b6643d77048eecdf6261cc0efb006b3e7ae1ff
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-01 05:52:16 +0900

    문제에서 요구하지 않는 사항 삭제

--- lib/2023_11_30/mild_taste/mild_taste_6.dart
+++ lib/2023_11_30/mild_taste/mild_taste_6.dart
@@ -1,25 +1,20 @@
 import 'dart:io';
 
 void main() {
-  String input = stdin.readLineSync() ?? '';
+  String input = stdin.readLineSync()!;
 
-  if ((int.tryParse(input)) != null) {
-    int i = 1;
+  int i = (input[0] != '-') ? 1 : 2;
 
-    while (i < input.length) {
-      if (input[i - 1] != input[i]) {
-        print('No');
-        break;
-      }
-
-      i++;
+  while (i < input.length) {
+    if (input[i - 1] != input[i]) {
+      print('No');
+      break;
     }
+    i++;
+  }
 
-    // 각 숫자가 같을 경우
-    if (i == input.length) {
-      print(input);
-    }
-  } else {
-    print('Not an integer');
+  // 각 숫자가 같을 경우
+  if (i == input.length) {
+    print(input);
   }
 }
