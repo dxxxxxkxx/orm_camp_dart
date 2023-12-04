bddba86fe3b23c2380fa77bc003540f0c1eeb420
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-05 02:46:36 +0900

    중간 맛 4, 5

---
+++ lib/2023_11_30/moderate_taste/n4_black_and_white.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main() {
+  final String input =
+      stdin.readLineSync()!.trim().replaceAll(RegExp(r'\s+'), ' ');
+  final List<int> hw = input.split(' ').map((e) => int.parse(e)).toList();
+
+  final int h = hw[0]; // height
+  final int w = hw[1]; // width
+
+  final List<List<int>> image = List.generate(
+      h, (index) => List.generate(w, (index) => 0, growable: false),
+      growable: false);
+
+  for (int i = 0; i < h; i++) {
+    final String input =
+        stdin.readLineSync()!.trim().replaceAll(RegExp(r'\s+'), ' ');
+    image[i] = input.split(' ').map((e) => int.parse(e)).toList();
+
+    for (int j = 0; j < w; j++) {
+      if (image[i][j] >= 0 && image[i][j] < 128) {
+        image[i][j] = 0;
+      } else if (image[i][j] >= 128 && image[i][j] < 256) {
+        image[i][j] = 1;
+      }
+    }
+  }
+
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w - 1; j++) {
+      stdout.write('${image[i][j]} ');
+    }
+    stdout.write('${image[i][w - 1]}\n');
+  }
+}
---
+++ lib/2023_11_30/moderate_taste/n5_valid_ipv4.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+void main() {
+  final int m = int.parse(stdin.readLineSync()!);
+  final List<String> ipv4s = [];
+
+  for (int i = 0; i < m; i++) {
+    ipv4s.add(stdin.readLineSync() ?? '');
+  }
+
+  for (var ipv4 in ipv4s) {
+    print(isValidIpv4(ipv4) ? 'True' : 'False');
+  }
+}
+
+bool isValidIpv4(String input) {
+  /*
+  * [0-1]?[0-9]{1,2} → 0 ~ 199
+  * 2[0-4][0-9] → 200 ~ 249
+  * 25[0-5] → 250 ~ 255
+  */
+  final RegExp regExp = RegExp(
+      r'^((([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))\.){3}([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5])$');
+
+  return regExp.hasMatch(input);
+}
