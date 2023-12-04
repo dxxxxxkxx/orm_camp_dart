0f383c7321ab698679974e94b8a61c4da54479ae
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-05 00:43:02 +0900

    문제에서 요구하지 않는 사항 삭제

--- lib/2023_11_30/mild_taste/mild_taste_5.dart
+++ lib/2023_11_30/mild_taste/mild_taste_5.dart
@@ -1,7 +1,7 @@
 void main() {
   const List<int> cards = [1, 2, 3, 4, 5];
-  List<int> collCards = [1, 3, 2, 5];
-  int? missCard;
+  const List<int> collCards = [1, 3, 2, 5];
+  int missCard = 0;
 
   for (int i = 0; i < cards.length; i++) {
     if (!collCards.contains(cards[i])) {
@@ -10,5 +10,5 @@ void main() {
     }
   }
 
-  print(missCard ?? 'No missing card');
+  print(missCard);
 }
