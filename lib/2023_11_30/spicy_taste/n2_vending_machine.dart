1afcedbdecb84e9a91c7b945d24d9ab6f0faa1dd
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-05 06:50:34 +0900

    매운 맛 2

---
+++ lib/2023_11_30/spicy_taste/n2_vending_machine.dart
@@ -0,0 +1,63 @@
+class Product {
+  final String name;
+  final int price;
+  int inventory;
+
+  Product({
+    required this.name,
+    required this.price,
+    this.inventory = 5,
+  });
+}
+
+class VendingMachine {
+  final List<Product> products;
+
+  const VendingMachine({
+    required this.products,
+  });
+
+  void showInfo() {
+    print('===== 자판기 상태 =====');
+
+    for (final Product product in products) {
+      print(
+          '${product.name} - 가격: ${product.price}원, 수량: ${product.inventory}개');
+    }
+
+    print('=====================');
+  }
+
+  void buyProduct(Product product, int money) {
+    // price 오름차순 정렬
+    products.sort((prev, next) => prev.price.compareTo(next.price));
+
+    for (int i = 0; i < products.length; i++) {
+      if ((products[i].inventory > 0) && (products[i].price <= money)) {
+        if ((product.inventory > 0) && (product.price <= money)) {
+          product.inventory--;
+          print(
+              '[${product.name} 구매 성공] 가격: ${product.price}원, 거스름돈: ${money - product.price}원, 남은 수량: ${product.inventory}개');
+        } else {
+          print('구매 실패');
+        }
+
+        break;
+      }
+    }
+  }
+}
+
+void main() {
+  final Product chocolate = Product(name: '초콜릿', price: 500);
+  final Product pepero = Product(name: '빼빼로', price: 600);
+  final Product fanta = Product(name: '환타', price: 1000);
+  final Product cider = Product(name: '사이다', price: 1100);
+
+  final VendingMachine vendingMachine =
+      VendingMachine(products: [chocolate, pepero, fanta, cider]);
+
+  vendingMachine.showInfo();
+  vendingMachine.buyProduct(pepero, 5000);
+  vendingMachine.showInfo();
+}
