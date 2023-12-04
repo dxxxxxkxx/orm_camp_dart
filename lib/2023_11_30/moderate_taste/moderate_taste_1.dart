a060ae57ee1a1ff2301e5246c8dda0e03f4fd9db
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-04 17:26:19 +0900

    중간 맛 1

--- lib/2023_11_30/moderate_taste/moderate_taste_1.dart
+++ lib/2023_11_30/moderate_taste/moderate_taste_1.dart
@@ -1,16 +1,15 @@
 import 'dart:io';
+import 'dart:math';
 
 void main() {
   // 공사 현장 위치 (a, b)
   int a = 0;
   int b = 0;
 
-  // 나무 그물 위치 (x, y)
-  int x = 0;
-  int y = 0;
-
-  int r = 0; // (a, b)와 (x, y) 사이의 거리
+  int r = 0; // 공사 현장 위치와 나무 그늘 위치 사이의 거리
   int n = 0; // 나무 그눌 수
+
+  List<String> result = [];
 
   String input = stdin.readLineSync()!.trim().replaceAll(RegExp(r'\s+'), ' ');
   List<int> abr = input.split(' ').map((e) => int.parse(e)).toList();
@@ -20,7 +19,21 @@ void main() {
 
   n = int.parse(stdin.readLineSync()!);
 
-  if (n > 0) {
-    for (int i = 0; i < n; i++) {}
+  for (int i = 0; i < n; i++) {
+    String input = stdin.readLineSync()!.trim().replaceAll(RegExp(r'\s+'), ' ');
+    List<int> xy = input.split(' ').map((e) => int.parse(e)).toList();
+
+    // 나무 그늘 위치 (x, y)
+    int x = xy[0];
+    int y = xy[1];
+
+    // (x, y)와 (a, b) 사이의 거리 >= r
+    if (pow(x - a, 2) + pow(y - b, 2) >= pow(r, 2)) {
+      result.add('silent');
+    } else {
+      result.add('noisy');
+    }
   }
+
+  result.forEach(print);
 }
