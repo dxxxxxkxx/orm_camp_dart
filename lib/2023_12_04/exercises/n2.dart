e164df29d101362d9d2e9b22e73f4bcac6a1b644
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-07 07:14:50 +0900

    연습 문제 2-3

--- lib/2023_12_04/exercises/n2.dart
+++ lib/2023_12_04/exercises/n2.dart
@@ -6,8 +6,15 @@ class Person {
 
 void main() {
   final List<Person> people = [Person(name: '홍길동'), Person(name: '한석봉')];
+  final Map<String, int> ageInfo = {people[0].name: 20, people[1].name: 25};
 
-  for (var e in people) {
-    print(e.name);
+  for (final Person person in people) {
+    print(person.name);
   }
+
+  print(''); // enter
+
+  ageInfo.forEach((final String name, final int age) {
+    print('$name의 나이는 $age살');
+  });
 }
