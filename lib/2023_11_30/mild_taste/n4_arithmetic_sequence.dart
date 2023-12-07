12ef3ec45e7809225e15bb62c68c8a8713ea7c98
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-07 12:25:10 +0900

    더 간단하게 수정

--- lib/2023_11_30/mild_taste/n4_arithmetic_sequence.dart
+++ lib/2023_11_30/mild_taste/n4_arithmetic_sequence.dart
@@ -1,20 +1,15 @@
-import 'dart:io';
-
 void main() {
   const int firstTerm = 5;
   const int commDiff = 10;
   const int num = 10;
 
-  final List<int> arithSeq = List.generate(num, (index) => 0, growable: false);
+  final List<int> arithSeq =
+      List.generate(num, (final int index) => 0, growable: false);
   arithSeq[0] = firstTerm;
 
   for (int i = 1; i < num; i++) {
     arithSeq[i] = arithSeq[i - 1] + commDiff;
   }
 
-  for (int i = 0; i < num - 1; i++) {
-    stdout.write('${arithSeq[i]} ');
-  }
-
-  stdout.write('${arithSeq[num - 1]}');
+  print(arithSeq.join(' ').trimRight());
 }
