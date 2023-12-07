78709a539cbfa831718ac230fc30b95152d3fb68
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-07 12:46:13 +0900

    불필요한 변수 삭제

--- lib/2023_11_30/mild_taste/n5_missing_card.dart
+++ lib/2023_11_30/mild_taste/n5_missing_card.dart
@@ -1,14 +1,11 @@
 void main() {
   const List<int> cards = [1, 2, 3, 4, 5];
   const List<int> collCards = [1, 3, 2, 5];
-  int missCard = 0;
 
   for (int i = 0; i < cards.length; i++) {
     if (!collCards.contains(cards[i])) {
-      missCard = cards[i];
+      print(cards[i]);
       break;
     }
   }
-
-  print(missCard);
 }
