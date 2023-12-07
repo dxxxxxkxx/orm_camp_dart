48854d6fd628753d9262be5e565b28c8579211b9
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-07 14:50:24 +0900

    연습 문제 10-2

--- lib/rpgGame/wand.dart
+++ lib/rpgGame/wand.dart
@@ -1,6 +1,41 @@
 class Wand {
-  String name;
-  double power;
+  String _name;
+  double _power;
 
-  Wand({required this.name, required this.power});
+  Wand({required String name, required double power})
+      : _name = name,
+        _power = power {
+    _validateName(_name);
+    _validatePower(_power);
+  }
+
+  // getter & setter ------------
+
+  String get name => _name;
+
+  set name(final String name) {
+    _validateName(name);
+    _name = name;
+  }
+
+  double get power => _power;
+
+  set power(final double power) {
+    _validatePower(power);
+    _power = power;
+  }
+
+  // -------------getter & setter
+
+  void _validateName(final String name) {
+    if (name.length < 3) {
+      throw Exception('_name은 null일 수 없고 3글자 이상이어야 한다');
+    }
+  }
+
+  void _validatePower(final double power) {
+    if (power < 0.5 || power > 100.0) {
+      throw Exception('_power는 0.5 이상 100.0 이하이어야 한다');
+    }
+  }
 }
--- lib/rpgGame/wizard.dart
+++ lib/rpgGame/wizard.dart
@@ -1,15 +1,60 @@
 import 'wand.dart';
 
 class Wizard {
-  String name;
-  int hp;
-  int mp;
+  String _name;
+  int _hp;
+  int _mp;
   Wand? wand;
 
   Wizard({
-    required this.name,
-    required this.hp,
-    required this.mp,
+    required final String name,
+    required final int hp,
+    required final int mp,
     this.wand,
-  });
+  })  : _name = name,
+        _hp = hp,
+        _mp = mp {
+    _validateName(_name);
+    _validateMp(_mp);
+  }
+
+  // getter & setter ----------
+
+  String get name => _name;
+
+  set name(final String name) {
+    _validateName(name);
+    _name = name;
+  }
+
+  int get hp => _hp;
+
+  set hp(final int hp) {
+    if (hp >= 0) {
+      _hp = hp;
+    } else {
+      _hp = 0;
+    }
+  }
+
+  int get mp => _mp;
+
+  set mp(final int mp) {
+    _validateMp(mp);
+    _mp = mp;
+  }
+
+  // ---------- getter & setter
+
+  void _validateName(final String name) {
+    if (name.length < 3) {
+      throw Exception('_name은 null일 수 없고 3글자 이상이어야 한다');
+    }
+  }
+
+  void _validateMp(final int mp) {
+    if (mp < 0) {
+      throw Exception('_mp는 0 이상이어야 한다');
+    }
+  }
 }
