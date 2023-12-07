3f272563f87d4258c34faae73648c253d4c24ede
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-08 02:45:05 +0900

    연습 문제 12

---
+++ lib/asset_mgmt/computer.dart
@@ -0,0 +1,13 @@
+class Computer {
+  String name;
+  int price;
+  String color;
+  String maker;
+
+  Computer({
+    required this.name,
+    required this.price,
+    required this.color,
+    required this.maker,
+  });
+}
