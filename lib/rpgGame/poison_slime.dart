25ce70aa1de4f63edfc899c4f0d0ea1305281dbe
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-07 23:00:28 +0900

    print 수정

--- lib/rpgGame/poison_slime.dart
+++ lib/rpgGame/poison_slime.dart
@@ -12,7 +12,7 @@ class PoisonSlime extends Slime {
 
     if (hero.hp > 0 && poisonCnt > 0) {
       final int damage = (hero.hp * 0.2).toInt();
-      print('독슬라임$suffix의 공격: 데미지 $damage');
+      print('독슬라임$suffix의 독 공격: 데미지 $damage');
       hero.hp -= damage;
       poisonCnt--;
     }
