98dc9d35461a4c276424e1918ceb024455a7afc4
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-11 14:49:04 +0900

    인터페이스의 getter, setter 활용하도록 수정

--- lib/asset_mgmt/tangible_asset.dart
+++ lib/asset_mgmt/tangible_asset.dart
@@ -2,14 +2,21 @@ import 'asset.dart';
 import 'thing.dart';
 
 abstract class TangibleAsset extends Asset implements Thing {
-  @override
-  double weight;
   String color;
+  double _weight;
 
   TangibleAsset({
     required super.name,
     required super.price,
-    required this.weight,
     required this.color,
-  });
+    required double weight,
+  }) : _weight = weight;
+
+  @override
+  double get weight => _weight;
+
+  @override
+  set weight(double weight) {
+    _weight = weight;
+  }
 }
