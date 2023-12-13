cc696c6214ed513b1fadf177d753cd4e9b3c141b
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-14 04:16:51 +0900

    연습 문제 3

---
+++ lib/2023_12_13/exercises/n3.dart
@@ -0,0 +1,9 @@
+Future<String> timeoutFuture() async {
+  await Future.delayed(const Duration(seconds: 6));
+  return 'OK';
+}
+
+void main() async {
+  print(await timeoutFuture()
+      .timeout(const Duration(seconds: 5), onTimeout: () => 'Timeout'));
+}
