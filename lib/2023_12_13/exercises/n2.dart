521fcc0bc53731007970b8f6f801379a6f81ba8a
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-14 11:28:07 +0900

    모델 클래스 사용

--- lib/2023_12_13/exercises/n2.dart
+++ lib/2023_12_13/exercises/n2.dart
@@ -1,5 +1,25 @@
 import 'dart:convert';
 
+class MovieInfo {
+  final String title;
+  final String director;
+  final int year;
+
+  const MovieInfo({
+    required this.title,
+    required this.director,
+    required this.year,
+  });
+
+  MovieInfo.fromJson(Map<String, dynamic> json)
+      : title = json['title'] as String,
+        director = json['director'] as String,
+        year = json['year'] as int;
+
+  Map<String, dynamic> toJson() =>
+      {'title': title, 'director': director, 'year': year};
+}
+
 Future<String> getMovieInfo() async {
   await Future.delayed(const Duration(seconds: 2));
   final Map<String, dynamic> mockData = {
@@ -12,6 +32,7 @@ Future<String> getMovieInfo() async {
 }
 
 void main() async {
-  final Map<String, dynamic> movieInfo = jsonDecode(await getMovieInfo());
-  print(movieInfo['director']);
+  final MovieInfo movieInfo =
+      MovieInfo.fromJson(jsonDecode(await getMovieInfo()));
+  print(movieInfo.director);
 }
