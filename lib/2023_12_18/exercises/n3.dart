918538cf2f1759a82e45b9dd0fd9ac87512a2f2a
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-19 01:52:42 +0900

    max 함수 사용

--- lib/2023_12_18/exercises/n3.dart
+++ lib/2023_12_18/exercises/n3.dart
@@ -1,6 +1,7 @@
+import 'dart:math';
+
 int? findMax({required final List<int> intList}) {
   return intList.isNotEmpty
-      ? intList
-          .reduce((final int prev, final int next) => prev > next ? prev : next)
+      ? intList.reduce((final int prev, final int next) => max(prev, next))
       : null;
 }
