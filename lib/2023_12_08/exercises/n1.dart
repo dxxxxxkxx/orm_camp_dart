51f272ec952e277c67d5a3b502dad1007d787c73
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-18 10:45:08 +0900

    연습 문제 2

--- lib/2023_12_08/exercises/n1.dart
+++ lib/2023_12_08/exercises/n1.dart
@@ -1,11 +1,53 @@
-class StrongBox<T> {
-  T? _instance;
+enum KeyType {
+  padlock(maxCount: 1024),
+  button(maxCount: 10000),
+  dial(maxCount: 30000),
+  finger(maxCount: 1000000);
 
-  StrongBox({T? instance}) : _instance = instance;
+  const KeyType({required this.maxCount});
 
-  T? get() => _instance;
+  final int maxCount;
+}
 
-  void set({required final T instance}) {
+class StrongBox<T extends KeyType> {
+  T _instance;
+  int _currentCount;
+
+  StrongBox({required final T instance})
+      : _instance = instance,
+        _currentCount = 0;
+
+  T? get() {
+    _currentCount++;
+
+    switch (_instance) {
+      case KeyType.padlock:
+        if (_currentCount < KeyType.padlock.maxCount) {
+          return null;
+        }
+        break;
+      case KeyType.button:
+        if (_currentCount < KeyType.button.maxCount) {
+          return null;
+        }
+        break;
+      case KeyType.dial:
+        if (_currentCount < KeyType.dial.maxCount) {
+          return null;
+        }
+        break;
+      case KeyType.finger:
+        if (_currentCount < KeyType.finger.maxCount) {
+          return null;
+        }
+        break;
+    }
+
+    return _instance;
+  }
+
+  void set({required final T instance, required final int currentCount}) {
     _instance = instance;
+    _currentCount = currentCount;
   }
 }
