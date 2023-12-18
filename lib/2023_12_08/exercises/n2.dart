f88f03295a9512263aa272a2cd8645c07221a5d6
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-18 10:46:51 +0900

    실습 문제 2

--- lib/2023_12_08/exercises/n2.dart
+++ lib/2023_12_08/exercises/n2.dart
@@ -7,4 +7,10 @@ class Word {
   bool? isVowel({required final int i}) {
     return i < word.length ? vowels.contains(word[i].toLowerCase()) : null;
   }
+
+  bool? isConsonant({required final int i}) {
+    final bool? result = isVowel(i: i);
+
+    return result != null ? (!result) : null;
+  }
 }
