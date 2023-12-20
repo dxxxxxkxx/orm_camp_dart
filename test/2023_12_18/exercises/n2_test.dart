9aa8117123ac9073bf346e057977c38ba8fe8538
dxxxxxkxx <dxxxxxkxx@gmail.com>
2023-12-20 13:26:45 +0900

    분류 수정

--- test/2023_12_18/exercises/n2_test.dart
+++ test/2023_12_18/exercises/n2_test.dart
@@ -2,19 +2,13 @@ import 'package:orm_camp_dart/2023_12_18/exercises/n2.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Even positive integer test', () {
+  test('Positive integer test', () {
     expect(isEven(num: 4), true);
-  });
-
-  test('Even negative integer test', () {
-    expect(isEven(num: -4), true);
-  });
-
-  test('Odd positive integer test', () {
     expect(isEven(num: 5), false);
   });
 
-  test('Odd negative integer test', () {
+  test('Negative integer test', () {
+    expect(isEven(num: -4), true);
     expect(isEven(num: -1), false);
   });
 
